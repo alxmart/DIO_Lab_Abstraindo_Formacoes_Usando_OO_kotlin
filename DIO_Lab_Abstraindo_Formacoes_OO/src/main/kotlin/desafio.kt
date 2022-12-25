4ebce48649ff0582acead42a27a7d0b23ef5a1f1
alxmart <alxmartinez@gmail.com>
2022-12-25 19:23:59 -0300

    Changed ConteudoEducacional = removed duracao.

--- DIO_Lab_Abstraindo_Formacoes_OO/src/main/kotlin/desafio.kt
+++ DIO_Lab_Abstraindo_Formacoes_OO/src/main/kotlin/desafio.kt
@@ -11,12 +11,16 @@ data class ConteudoEducacional(
     val nome: String
 )
 
+
+
+
 data class Formacao(
     val nome: String,
     var conteudos: List<ConteudoEducacional>
 ) {
     val inscritos = mutableListOf<Usuario>()
     fun matricular(usuario: Usuario) {
+
 //        TODO("Utilize o parâmetro $usuario para simular uma matrícula " +
 //                "(usar a lista de $inscritos).")
 
